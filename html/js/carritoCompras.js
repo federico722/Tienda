7fab01bac4f329f1b0d74815625a7860a6f52266
federico722 <federicopelaezcalderon@gmail.com>
2023-07-13 08:25:17 -0500

    carrito de compra

--- html/js/carritoCompras.js
+++ html/js/carritoCompras.js
@@ -1,6 +1,16 @@
-function agregarCantidad(image) {
+/*function agregarCantidad(image) {
     let cantidadElemento = image.nextElementSibling;
     let cantidadActual = parseInt(cantidadElemento.innerText);
     cantidadActual++;
+    cantidadElemento.innerText = cantidadActual;
+}
+*/
+
+
+
+function actualizarCantidad(cantidadId, incremento) {
+    let cantidadElemento = document.getElementById(cantidadId);
+    let cantidadActual = parseInt(cantidadElemento.innerText);
+    cantidadActual += incremento;
     cantidadElemento.innerText = cantidadActual;
 }
